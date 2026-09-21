a81af8fc833b47aa83af02b99e9ad659d6887b6f
imran2unkown <yusufimran310@gmail.com>
2026-09-21 14:13:32 +0300

    Initial commit

---
+++ students.py
@@ -0,0 +1,12 @@
+school="Dazu institution"
+studentname="imran ahmed"
+location="Nairobi"
+building="siaya house"
+age=21
+name="imran ahmed"
+print(school,"dazu institution")
+print(studentname,"imran ahmed")
+print(location,"Nairobi")
+print(building,"siaya house")
+print(age,"21")
+print(name,"imran ahmed")
---
+++ variables.py
@@ -0,0 +1,7 @@
+name="imran ahmed"
+print(name)
+school="Dagoretti high school"
+date='2026-21-09'
+age=20
+print(school,name,date,age)
+print("my school is",school,"my name is",name,"my age is",age,"the date is",date)
